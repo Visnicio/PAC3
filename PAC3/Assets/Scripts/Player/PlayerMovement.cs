6608fadf70e24c2cae1a83b37d8dc8b3ad208301
Visnicio <vinicius.antonio0712@gmail.com>
2022-04-05 20:08:28 -0300

    Adição da movimentação
    Controles do personagem estabelecidos

---
+++ PAC3/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed = 15f;
+    private Rigidbody rb;
+
+     Vector3 moveDirection;
+
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked; 
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Vertical");
+        float inputZ = Input.GetAxisRaw("Horizontal");
+
+        moveDirection = transform.forward * inputX + transform.right * inputZ;
+
+        rb.velocity = new Vector3(moveDirection.x * speed, rb.velocity.y, moveDirection.z * speed);
+    }
+}
